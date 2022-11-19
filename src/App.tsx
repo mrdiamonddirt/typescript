87a9e31be6f29704e0aa99afc394ccc660c9c2a7
RowDog <mrdiamonddirt@gmail.com>
2022-11-19 08:52:55 +0000

    api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
 import MyHeader from './MyHeader';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [ApiResponse, setSetApiResponse] = useState()
+  const [pokeurlnumber, setpokeurlnumber] = useState('')
+ 
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeurlnumber}`)
+      .then(response => response.json())
+      .then(data => setSetApiResponse(data.results));
+      
+    return () => {
+      
+    }
+  }, [pokeurlnumber])
+  
+
   return (
     <>
     <div className="App">
